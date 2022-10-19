2a94f47ed87ccd5e88a420d93e6f98c648b53aa3
Jake <boxjack.romero@gmail.com>
2022-10-19 00:52:41 -0300

    feat: adds winicon patch

--- config.def.h
+++ config.def.h
@@ -1,4 +1,6 @@
 /* See LICENSE file for copyright and license details. */
+#define ICONSIZE 16   /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
